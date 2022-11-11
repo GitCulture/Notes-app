c387663a4774d9c9f29722d796be551b392df374
Konrad Koniec <konradczerniec@gmail.com>
2022-11-11 10:50:52 +0100

    notes app / first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+import './global.css';
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <head />
+      <body>
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/notes">Notes</Link>
+          </nav>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>Some content</p>
+    </>
+  );
+}
+
+export default HomePage;
